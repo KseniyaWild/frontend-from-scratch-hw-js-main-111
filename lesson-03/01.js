865fa0ffdc0e6cd004733a0debeb33c8e3ef8b6d
KseniyaWild <investfuture2023@mail.ru>
2025-04-12 21:34:14 +0300

    lesson-03/01.js extra parentheses for return

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,9 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
 function calculateAverage(a, b) {
-    return (a + b) / 2;
+    return ((a + b) / 2);
 }
 
-calculateAverage(4, 8);
-console.log(calculateAverage(4,8));
+let result = calculateAverage(4, 8);
+console.log(result);
 
