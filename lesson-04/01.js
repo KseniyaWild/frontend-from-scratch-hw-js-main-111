8c5e1b47942de7d1311ada24fd55d9ef6716be5c
KseniyaWild <investfuture2023@mail.ru>
2025-04-22 23:02:27 +0300

    lesson-04/01.js

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,17 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+const elements = [1, 2, 3, 4];
+
+function includesElement(array, element) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === element){
+            return true;
+        }
+    }
+    return false;
+}
+
+includesElement(elements, 3);
+
+
