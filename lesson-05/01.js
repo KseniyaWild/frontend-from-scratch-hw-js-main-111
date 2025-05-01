18730ba0b046e357c9ca75102c13959fce7b7936
KseniyaWild <investfuture2023@mail.ru>
2025-05-01 17:13:57 +0300

    lesson-05/01.js

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    if(!game.resources) {
+      return console.log('Invalid resource');
+    }
+    game.resources[resource] += amount;
+  }
 }
+
