c015e158c84116363b87d5a20ad5ee0a52d6f4f8
KseniyaWild <investfuture2023@mail.ru>
2025-05-08 14:46:47 +0300

    lesson-06/01.js

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -17,3 +17,27 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+let prevBtn = document.getElementById('prev-button');
+let nextBtn = document.getElementById('next-button');
+let imgMain = document.getElementById('web-tech-image');
+
+let index = 0;
+
+function updateImages() {
+imgMain.src = WEB_TECH_IMAGES[index]
+}
+
+prevBtn.addEventListener('click', function () {
+
+  index = (index - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  updateImages();
+})
+
+nextBtn.addEventListener('click', function () {
+
+  index = (index + 1) % WEB_TECH_IMAGES.length; // Переход к следующему изображению
+  updateImages();
+})
+
+updateImages();
